2d4770a2a3071a106ab53aab86d2315d256d9de9
sdkayy <sdkay@sdkay.pw>
2018-01-12 22:56:18 -0500

    Testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,8 +31,8 @@ body, html {
     <a href="#home" class="w3-bar-item w3-button w3-wide">Authed</a>
     <!-- Right-sided navbar links -->
     <div class="w3-right w3-hide-small">
-      <a href="#about" class="w3-bar-item w3-button"><i class="fa fa-user"></i> LOGIN</a>
-      <a href="#team" class="w3-bar-item w3-button"><i class="fa fa-user-plus"></i> REGISTER</a>
+      <a href="login" class="w3-bar-item w3-button"><i class="fa fa-user"></i> LOGIN</a>
+      <a href="register" class="w3-bar-item w3-button"><i class="fa fa-user-plus"></i> REGISTER</a>
     </div>
     <!-- Hide right-floated links on small screens and replace them with a menu icon -->
 
