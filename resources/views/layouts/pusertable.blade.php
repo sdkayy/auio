098403709a0385091155989c41b7cdec84c330ad
SDK <sdkay@sdkay.pw>
2018-03-07 11:58:32 -0500

    Fixed Buttons cut off

--- resources/views/layouts/pusertable.blade.php
+++ resources/views/layouts/pusertable.blade.php
@@ -2,7 +2,7 @@
 <td>{{ $user->username }}</td>
 <td>
 	<p id="userMail-{{ $user->id }}" style="display:none" onclick="hideagain('{{ $user->id }}')">{{ $user->email }}</p>
-  	<button class="w3-button w3-half sdk" id="hide-{{ $user->id }}" onclick="reveal('{{ $user->id }}')">Click to reveal user email</button>
+  	<button class="w3-button sdk" id="hide-{{ $user->id }}" onclick="reveal('{{ $user->id }}')">Click to reveal user email</button>
 </td>
 <td>{{ $user->created_at->toFormattedDateString() }}</td>
 <td>{{ $user->expires->toFormattedDateString() }}</td>
